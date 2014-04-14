d6088ddedc9fe67a2f470c0cbff5b7cac01287f8
Manuel Hervo <manuel.hervo@gmail.com>
2014-04-14 20:38:23 +0200

    Add new dependencies in app angular

--- public/init.js
+++ public/init.js
@@ -2,15 +2,24 @@
 
 angular.element(document).ready(function() {
 	//Fixing facebook bug with redirect
-	if (window.location.hash === '#_=_') window.location.hash = '#!';
+	//if (window.location.hash === '#_=_') window.location.hash = '#!';
 
 	//Then init the app
-	angular.bootstrap(document, ['wcb']);
+	//angular.bootstrap(document, ['wcb']);
 
 });
 
 // Default modules
-var modules = ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router', 'wcb.system', 'wcb.teams', 'wcb.matches', 'wcb.auth'];
+var modules = ['ngRoute', 'ngLocale', 'ngCookies', 'ngResource', 'ui.bootstrap', 'wcb.system', 'wcb.auth', 'wcb.matches', 'wcb.teams'];
 
 // Combined modules
-angular.module('wcb', modules);
+angular.module('wcb', modules)
+  .config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    $locationProvider.html5Mode(true);
+    $routeProvider.otherwise({redirectTo:'/'});
+  }])
+  .run(['security', function(security) {
+    // Get the current user when the application starts
+    // (in case they are still logged in from a previous session)
+    security.requestCurrentUser();
+  }]);
