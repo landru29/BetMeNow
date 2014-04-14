c4eb0576764e619c015c3500bcd63676b5fa6740
Manuel Hervo <manuel.hervo@gmail.com>
2014-04-14 20:35:37 +0200

    Change serve static files in express config

--- server/config/express.js
+++ server/config/express.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 var express = require('express'),
-	consolidate = require('consolidate'),
+  consolidate = require('consolidate'),
     mongoStore = require('connect-mongo')(express),
     flash = require('connect-flash'),
     helpers = require('view-helpers'),
@@ -19,27 +19,33 @@ module.exports = function(app, passport, db) {
     // cache=memory or swig dies in NODE_ENV=production
     app.locals.cache = 'memory';
 
+    // Setting the fav icon and static folder
+    app.use(express.favicon(config.root + '/public/auth/assets/img/icons/favicon.ico'));
     // Should be placed before express.static
     // To ensure that all assets and data are compressed (utilize bandwidth)
-    app.use(express.compress({
-        filter: function(req, res) {
-            return (/json|text|javascript|css/).test(res.getHeader('Content-Type'));
-        },
-        // Levels are specified in a range of 0 to 9, where-as 0 is
-        // no compression and 9 is best compression, but slowest
-        level: 9
+    app.use('/public', express.compress({
+      filter: function(req, res) {
+        return (/json|text|javascript|css/).test(res.getHeader('Content-Type'));
+      },
+      // Levels are specified in a range of 0 to 9, where-as 0 is
+      // no compression and 9 is best compression, but slowest
+      level: 9
     }));
+    app.use('/public', express.static(config.root + '/public'));
+    app.use('/public', function(req, res) {
+      res.send(404); // If we get here then the request for a static file is invalid
+    });
 
     // Only use logger for development environment
     if (process.env.NODE_ENV === 'development') {
-		// Prettify HTML
-		app.locals.pretty = true;
-		app.set('showStackError', true);
+    // Prettify HTML
+    app.locals.pretty = true;
+    app.set('showStackError', true);
         app.use(express.logger('dev'));
     }
 
-	// assign the template engine to .html files
-	app.engine('html', consolidate[config.templateEngine]);
+  // assign the template engine to .html files
+  app.engine('html', consolidate[config.templateEngine]);
 
     // set .html as the default extension
     app.set('view engine', 'html');
@@ -66,7 +72,7 @@ module.exports = function(app, passport, db) {
             js: assets.js,
             css: assets.css,
             debug: (process.env.NODE_ENV !== 'production'),
-            webroot: 'public/public'
+            webroot: '(public/)'
         });
         // Add assets to local variables
         app.use(function(req, res, next) {
@@ -98,51 +104,59 @@ module.exports = function(app, passport, db) {
         // Routes should be at the last
         app.use(app.router);
 
-        // Setting the fav icon and static folder
-        app.use(express.favicon(config.root + '/public/auth/assets/img/icons/favicon.ico'));
-        app.use('/public', express.static(config.root + '/public'));
-
         var routes_path = appPath + '/server/routes';
-        var walk = function(path) {
-            fs.readdirSync(path).forEach(function(file) {
-                var newPath = path + '/' + file;
-                var stat = fs.statSync(newPath);
-                if (stat.isFile()) {
-                    if (/\.js$/.test(file)) {
-                        require(newPath)(app, passport);
-                    }
-                    // We skip the app/routes/middlewares directory as it is meant to be
-                    // used and shared by routes as further middlewares and is not a
-                    // route by itself
-                } else if (stat.isDirectory() && file !== 'middlewares') {
-                    walk(newPath);
-                }
-            });
+        var walk = function(path, done) {
+          var files = fs.readdirSync(path),
+              nbFiles = files.length;
+          files.forEach(function(file, index) {
+            var newPath = path + '/' + file;
+            var stat = fs.statSync(newPath);
+            if (stat.isFile()) {
+              if (/\.js$/.test(file)) {
+                require(newPath)(app, passport);
+              }
+              // We skip the app/routes/middlewares directory as it is meant to be
+              // used and shared by routes as further middlewares and is not a
+              // route by itself
+            } else if (stat.isDirectory() && file !== 'middlewares') {
+              walk(newPath);
+            }
+            if (index === nbFiles-1) {
+              done();
+            }
+          });
         };
-        walk(routes_path);
+        walk(routes_path, function() {
+          app.all('/*', function (req, res) {
+            res.render('index', {
+              user: req.user ? JSON.stringify(req.user.name) : 'null',
+              roles: req.user ? JSON.stringify(req.user.roles) : JSON.stringify(['annonymous'])
+            });
+          });
+        });
 
-		// Assume "not found" in the error msgs is a 404. this is somewhat
-		// silly, but valid, you can do whatever you like, set properties,
-		// use instanceof etc.
-		app.use(function(err, req, res, next) {
-			// Treat as 404
-			if (~err.message.indexOf('not found')) return next();
+    // Assume "not found" in the error msgs is a 404. this is somewhat
+    // silly, but valid, you can do whatever you like, set properties,
+    // use instanceof etc.
+    app.use(function(err, req, res, next) {
+      // Treat as 404
+      if (~err.message.indexOf('not found')) return next();
 
-			// Log it
-			console.error(err.stack);
+      // Log it
+      console.error(err.stack);
 
-			// Error page
-			res.status(500).render('500', {
-				error: err.stack
-			});
-		});
+      // Error page
+      res.status(500).render('500', {
+        error: err.stack
+      });
+    });
 
-		// Assume 404 since no middleware responded
-		app.use(function(req, res) {
-			res.status(404).render('404', {
-				url: req.originalUrl,
-				error: 'Not found'
-			});
-		});
+    // Assume 404 since no middleware responded
+    app.use(function(req, res) {
+      res.status(404).render('404', {
+        url: req.originalUrl,
+        error: 'Not found'
+      });
+    });
     });
 };
