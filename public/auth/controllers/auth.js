4a5a9cb815450b030009ce2373ac3e7390af6941
Manuel Hervo <manuel.hervo@gmail.com>
2014-04-14 20:28:53 +0200

    Change actions login and register

--- public/auth/controllers/auth.js
+++ public/auth/controllers/auth.js
@@ -1,57 +1,86 @@
 'use strict';
 
-angular.module('wcb.controllers.login', [])
-    .controller('LoginCtrl', ['$scope','$rootScope','$http','$location', function($scope, $rootScope, $http, $location) {
-        // This object will be filled by the form
-        $scope.user = {};
+angular.module('wcb.auth.login', [/*'wcb.system.localizedMessages', */'wcb.auth.security'])
+  .controller('LoginCtrlBis', ['$scope', 'security'/*, 'localizedMessages'*/, function($scope, security/*, localizedMessages*/) {
+    // The model for this form
+    $scope.user = {};
 
-        // Register the login() function
-        $scope.login = function(){
-            $http.post('/login', {
-                email: $scope.user.email,
-                password: $scope.user.password
-            })
-                .success(function(user){
-                    // authentication OK
-                    $scope.loginError = 0;
-                    $rootScope.user = user;
-                    $rootScope.$emit('loggedin');
-                    $location.url('/');
-                })
-                .error(function() {
-                    $scope.loginerror = 'Authentication failed.';
-                });
-        };
-    }])
-    .controller('RegisterCtrl', ['$scope','$rootScope','$http','$location', function($scope, $rootScope, $http, $location) {
-        $scope.user = {};
+    // Any error message from failing to login
+    $scope.authError = null;
 
-        $scope.register = function(){
-            $scope.usernameError = null;
-            $scope.registerError = null;
-            $http.post('/register', {
-                email: $scope.user.email,
-                password: $scope.user.password,
-                confirmPassword: $scope.user.confirmPassword,
-                username: $scope.user.username,
-                name: $scope.user.fullname
-            })
-                .success(function(){
-                    // authentication OK
-                    $scope.registerError = 0;
-                    $rootScope.user = $scope.user.fullname;
-                    $rootScope.$emit('loggedin');
-                    $location.url('/');
+    // The reason that we are being asked to login - for instance because we tried to access something to which we are not authorized
+    // We could do something diffent for each reason here but to keep it simple...
+    $scope.authReason = null;
+    if ( security.getLoginReason() ) {
+      $scope.authReason = ( security.isAuthenticated() ) ?
+        /*localizedMessages.get(*/'login.reason.notAuthorized'/*)*/ :
+        /*localizedMessages.get(*/'login.reason.notAuthenticated'/*)*/;
+    }
 
-                })
-                .error(function(error){
-                    // Error: authentication failed
-                    if (error === 'Username already taken') {
-                        $scope.usernameError = error;
-                    }
-                    else {
-                        $scope.registerError = error;
-                    }
-                });
-        };
-    }]);
+    // Attempt to authenticate the user specified in the form's model
+    $scope.login = function() {
+      // Clear any previous security errors
+      $scope.authError = null;
+
+      // Try to login
+      security.login($scope.user.email, $scope.user.password)
+        .then(function(loggedIn) {
+          if ( !loggedIn ) {
+            // If we get here then the login failed due to bad credentials
+            $scope.authError = /*localizedMessages.get(*/'login.error.invalidCredentials'/*)*/;
+          }
+        }, function(x) {
+          // If we get here then there was a problem with the login request to the server
+          $scope.authError = /*localizedMessages.get(*/'login.error.serverError: ' + x/*, { exception: x })*/;
+        });
+    };
+
+    $scope.clearForm = function() {
+      $scope.user = {};
+    };
+
+    $scope.cancelLogin = function() {
+      security.cancelLogin();
+    };
+
+    $scope.register = function() {
+      security.showSignup();
+    };
+  }])
+  .controller('RegisterCtrl', ['$scope','$rootScope','$http','$location', 'security', function($scope, $rootScope, $http, $location, security) {
+    $scope.user = {};
+
+    // Attempt to authenticate the user specified in the form's model
+    $scope.register = function() {
+      $scope.usernameError = null;
+      $scope.registerError = null;
+      // Clear any previous security errors
+      $scope.authError = null;
+
+      // Try to login
+      security.register({
+        email: $scope.user.email, 
+        password: $scope.user.password,
+        confirmPassword: $scope.user.confirmPassword,
+        username: $scope.user.username,
+        name: $scope.user.fullname
+      })
+        .then(null, function(error) {
+          // Error: authentication failed
+          if (error === 'Username already taken') {
+            $scope.usernameError = error;
+          }
+          else {
+            $scope.registerError = error;
+          }
+        });
+    };
+
+    $scope.closeRegister = function() {
+      security.cancelRegister();
+    };
+
+    $scope.login = function() {
+      security.showLogin();
+    };
+}]);
--- public/auth/routes/auth.js
+++ public/auth/routes/auth.js
@@ -1,78 +1,17 @@
 'use strict';
 
 //Setting up route
-angular.module('wcb.auth').config(['$stateProvider', '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
+angular.module('wcb.auth').config(['$routeProvider', 'securityAuthorizationProvider',
+  function ($routeProvider, securityAuthorizationProvider) {
 
-        //================================================
-        // Check if the user is connected
-        //================================================
-        var checkLoggedin = function($q, $timeout, $http, $location) {
-            // Initialize a new promise
-            var deferred = $q.defer();
+    $routeProvider.when('/login', {
+      templateUrl:'/public/auth/views/login.html',
+      controller:'LoginCtrl',
+    });
 
-            // Make an AJAX call to check if the user is logged in
-            $http.get('/loggedin').success(function(user) {
-                // Authenticated
-                if (user !== '0')
-                    $timeout(deferred.resolve, 0);
-
-                // Not Authenticated
-                else {
-                    $timeout(function() {
-                        deferred.reject();
-                    }, 0);
-                    $location.url('/login');
-                }
-            });
-
-            return deferred.promise;
-        };
-        //================================================
-        // Check if the user is not conntect
-        //================================================
-        var checkLoggedOut = function($q, $timeout, $http, $location) {
-            // Initialize a new promise
-            var deferred = $q.defer();
-
-            // Make an AJAX call to check if the user is logged in
-            $http.get('/loggedin').success(function(user) {
-                // Authenticated
-                if (user !== '0') {
-                    $timeout(function() {
-                        deferred.reject();
-                    }, 0);
-                    $location.url('/login');
-
-                }
-
-                // Not Authenticated
-                else {
-                    $timeout(deferred.resolve, 0);
-
-                }
-            });
-
-            return deferred.promise;
-        };
-        //================================================
-
-
-        // states for my app
-        $stateProvider
-            .state('auth.login', {
-                url: '/login',
-                templateUrl: 'public/auth/views/login.html',
-                resolve: {
-                    loggedin: checkLoggedOut
-                }
-            })
-            .state('auth.register', {
-                url: '/register',
-                templateUrl: 'public/auth/views/register.html',
-                resolve: {
-                    loggedin: checkLoggedOut
-                }
-            });
-    }
+    $routeProvider.when('/register', {
+      templateUrl:'/public/auth/views/register.html',
+      controller:'RegisterCtrl'
+    });
+  }
 ]);
