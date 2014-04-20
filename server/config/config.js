1182522096eddbb32efd1547d28756beaf098771
Manuel Hervo <manuel.hervo@gmail.com>
2014-04-20 13:24:41 +0200

    Change config file

--- server/config/config.js
+++ server/config/config.js
@@ -2,11 +2,12 @@
 
 // Utilize Lo-Dash utility library
 var _ = require('lodash'),
-	fs = require('fs');
+	fs = require('fs'),
+  configPath = dirname(__file);
 
 // Load configurations
 // Set the node environment variable if not set before
-process.env.NODE_ENV = ~fs.readdirSync('./server/config/env').map(function(file) {
+process.env.NODE_ENV = ~fs.readdirSync(configPath + '/env').map(function(file) {
 	return file.slice(0, -3);
 }).indexOf(process.env.NODE_ENV) ? process.env.NODE_ENV : 'development';
 
